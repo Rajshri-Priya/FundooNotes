32e272248ac3db14c2c46d0364de8e8f72e315a7
rajshri-priya <rajshri.priya1708@.gmail.com>
2023-02-01 16:56:06 +0530

    [Rajshri-Priya]Added Notes App

--- FundooNotes/urls.py
+++ FundooNotes/urls.py
@@ -19,6 +19,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('user_auth.urls')),
+    path('user/', include('user_auth.urls')),
+    path('notes/', include('Notes.urls')),
 
 ]

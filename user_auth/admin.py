6d5c7e075e7413212ddc22e57244a4cd80f48bb0
rajshri-priya <rajshri.priya1708@.gmail.com>
2023-02-02 22:47:17 +0530

    [Rajshri-Priya]Added register user_auth with admin

--- user_auth/admin.py
+++ user_auth/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
 from user_auth.models import CustomUser
 
 # Register your models here.
 
 admin.site.register(CustomUser)
+# class User(UserAdmin):

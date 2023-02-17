f938b55589b1b272b74f8f41aa0c59a0fa711560
rajshri-priya <rajshri.priya1708@.gmail.com>
2023-02-17 09:25:59 +0530

    [Rajshri-Priya]refactor urls acc to id take in url Notes

--- Notes/urls.py
+++ Notes/urls.py
@@ -3,5 +3,13 @@ from django.urls import path
 from Notes import views
 
 urlpatterns = [
-    path('note/', views.NotesAPIView.as_view(), name='Notes'),
+    path('note', views.NotesAPIView.as_view(), name='Notes'),
+    path('note/<int:note_id>/', views.NotesAPIView.as_view(), name='Notes_update_delete'),
+    path('label', views.LabelsAPIView.as_view(), name='label'),
+    path('label/<int:note_id>/', views.LabelsAPIView.as_view(), name='label'),
+    path('archive', views.ArchiveNoteList.as_view(), name='archive'),
+    path('archive/<int:note_id>/', views.ArchiveNoteList.as_view(), name='archive-update'),
+    path('trash', views.TrashNotesAPIView.as_view(), name='trash'),
+    path('trash/<int:note_id>/', views.TrashNotesAPIView.as_view(), name='trash-update'),
+
 ]

845d57cce1da65862584237c06c8e1e44498b8aa
rajshri-priya <rajshri.priya1708@.gmail.com>
2023-02-17 14:44:28 +0530

    [Rajshri-Priya] Added regarding celery

--- FundooNotes/celery.py
+++ FundooNotes/celery.py
@@ -13,16 +13,10 @@ app.config_from_object(settings, namespace='CELERY')
 
 # Celery Beat Settings
 app.conf.beat_schedule = {
-    # 'send-mail-every-day-at-8':{
-    #     'action':'email_app.tasks.send_mail_func',
-    #     'schedule':crontab(hour=1, minute=40),
-    #      # 'args':(2,)
+
     }
 
 app.autodiscover_tasks()
-
-
 @app.task(bind=True)
-
 def debug_task(self):
     print(f'Request: {self.request!r}')

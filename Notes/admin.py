52f3dc872ddef94c777d41997ff1b46f7727e675
rajshri-priya <rajshri.priya1708@.gmail.com>
2023-02-17 09:30:06 +0530

    [Rajshri-Priya]

--- Notes/admin.py
+++ Notes/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from Notes.models import Notes
+from Notes.models import Notes, Labels
 
 # Register your models here.
 admin.site.register(Notes)
+admin.site.register(Labels)

bd6cfc8a7cd2680c2a74841c55ed390e87e6997e
rajshri-priya <rajshri.priya1708@.gmail.com>
2023-02-17 14:46:28 +0530

    [Rajshri-Priya] refactor urls acc.to id for put and del

--- user_auth/urls.py
+++ user_auth/urls.py
@@ -4,6 +4,7 @@ from user_auth import views
 
 urlpatterns = [
     path('signup/', views.CustomUserRegistrationAPIView.as_view(), name='list'),
+    path('signup/<int:pk>', views.CustomUserRegistrationAPIView.as_view(), name='registeration(update/delete)'),
     path('login/', views.CustomUserLoginAPIView.as_view(), name='login'),
 
 ]

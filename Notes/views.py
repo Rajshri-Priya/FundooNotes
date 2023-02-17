1b1b257f69c102cad7b06bc4c70a32fba7779045
rajshri-priya <rajshri.priya1708@.gmail.com>
2023-02-18 01:26:46 +0530

    [Rajshri-Priya] Added logging config-> .logger file

--- Notes/views.py
+++ Notes/views.py
@@ -18,20 +18,24 @@ class NotesAPIView(APIView):
             serializer = NotesSerializer(data=request.data)
             serializer.is_valid(raise_exception=True)
             serializer.save()
-            return Response({"message": "Note Created Successfully", "data": serializer.data, "status": 201},
-                            status=201)
+            return Response(
+                {"success": True, "message": "Note Created Successfully", "data": serializer.data, "status": 201},
+                status=201)
         except Exception as e:
+            # logger.exception(e.args[0])==> print specific exception message
             logger.exception(e)
-            return Response({'message': str(e)}, status=400)
+            return Response({"success": False, "message": str(e), "status": 400}, status=400)
 
     def get(self, request):
         try:
             notes = Notes.objects.filter(user=request.user)
             serializer = NotesSerializer(notes, many=True)
-            return Response(serializer.data)
+            return Response(
+                {"success": True, "message": "Note Retrieved Successfully", "data": serializer.data, "status": 201},
+                status=201)
         except Exception as e:
             logger.exception(e)
-            return Response({'message': str(e)}, status=400)
+            return Response({"success": False, "message": str(e), "status": 400}, status=400)
 
     def put(self, request, note_id):
         try:
@@ -40,20 +44,21 @@ class NotesAPIView(APIView):
             serializer = NotesSerializer(notes, data=request.data)
             serializer.is_valid(raise_exception=True)
             serializer.save()
-            return Response({'message': 'Note updated successfully!', 'Data': serializer.data})
+            return Response({"success": True, 'message': 'Note updated successfully!', 'Data': serializer.data,
+                             "status": 201}, status=201)
 
         except Exception as e:
             logger.exception(e)
-            return Response({'message': str(e)}, status=400)
+            return Response({"success": False, "message": str(e), "status": 400}, status=400)
 
     def delete(self, request, note_id):
         try:
             notes = Notes.objects.get(id=note_id)
             notes.delete()
-            return Response({"Message": "Note Deleted Successfully"}, status=204)
+            return Response({"success": True, "Message": "Note Deleted Successfully", "status": 204}, status=204)
         except Exception as e:
             logger.exception(e)
-            return Response({'message': str(e)}, status=400)
+            return Response({"success": False, "message": str(e), "status": 400}, status=400)
 
 
 class LabelsAPIView(APIView):
@@ -64,41 +69,42 @@ class LabelsAPIView(APIView):
             serializer = LabelsSerializer(data=request.data)
             serializer.is_valid(raise_exception=True)
             serializer.save()
-            return Response({"message": "Label Added Successfully", "data": serializer.data, "status": 201}, status=201)
+            return Response({"success": True, "message": "Label Added Successfully", "data": serializer.data,
+                             "status": 201}, status=201)
         except Exception as e:
             logger.exception(e)
-            return Response({'message': str(e)}, status=400)
+            return Response({"success": False, "message": str(e), "status": 400}, status=400)
 
     def get(self, request):
         try:
             labels = Labels.objects.filter(user=request.user)
             serializer = LabelsSerializer(labels, many=True)
-            return Response({"message": "Label retrieve Successfully", "data": serializer.data, "status": 201},
-                            status=201)
+            return Response({"success": True, "message": "Label retrieve Successfully", "data": serializer.data,
+                             "status": 201}, status=201)
         except Exception as e:
             logger.exception(e)
-            return Response({'message': str(e)}, status=400)
+            return Response({"success": False, "message": str(e), "status": 400}, status=400)
 
     def put(self, request, labels):
         try:
             serializer = LabelsSerializer(labels, data=request.data)
             serializer.is_valid(raise_exception=True)
             serializer.save()
-            return Response({'message': 'Labels updated successfully!', 'Data': serializer.data, 'status': 201},
-                            status=201)
+            return Response({"success": True, 'message': 'Labels updated successfully!', 'Data': serializer.data,
+                             'status': 201}, status=201)
 
         except Exception as e:
             logger.exception(e)
-            return Response({'message': str(e)}, status=400)
+            return Response({"success": False, "message": str(e), "status": 400}, status=400)
 
     def delete(self, request, pk):
         try:
             labels = Labels.objects.get(id=pk)
             labels.delete()
-            return Response({"Message": "Labels Deleted Successfully"}, status=204)
+            return Response({"success": True, "Message": "Labels Deleted Successfully"}, status=204)
         except Exception as e:
             logger.exception(e)
-            return Response({'message': str(e)}, status=400)
+            return Response({"success": False, "message": str(e), "status": 400}, status=400)
 
 
 class ArchiveNoteList(APIView):
@@ -113,28 +119,28 @@ class ArchiveNoteList(APIView):
 
             else:
                 notes.isArchive = False
-                return Response({'message': 'isArchived updated not successfully!'})
+                return Response({"success": False, 'message': 'isArchived updated not successfully!'}, status=200)
             notes.save()
-            return Response({'message': 'isArchived updated successfully!'})
+            return Response({"success": True, 'message': 'isArchived updated successfully!'}, status=200)
         except Exception as e:
             logger.exception(e)
-            return Response({'message': str(e)}, status=400)
+            return Response({"success": False, "message": str(e), "status": 400}, status=400)
 
     def get(self, request):
         try:
             notes = Notes.objects.filter(isArchive=True)
             serializer = NotesSerializer(notes, many=True)
-            return Response(serializer.data)
+            return Response({"success": True, 'message': 'isArchived retrieve successfully!', 'Data': serializer.data,
+                             'status': 201}, status=201)
         except Exception as e:
             logger.exception(e)
-            return Response({'message': str(e)}, status=400)
+            return Response({"success": False, "message": str(e), "status": 400}, status=400)
 
 
 class TrashNotesAPIView(APIView):
 
     def put(self, request, note_id):
         try:
-            # note_id = request.data.get('id')
             notes = Notes.objects.get(id=note_id)
             if notes.isTrash == False:
                 notes.isTrash = True
@@ -145,13 +151,14 @@ class TrashNotesAPIView(APIView):
             return Response({'success': True, 'message': 'Notes isTrash successful!'}, status=200)
         except Exception as e:
             logger.exception(e)
-            return Response({'message': str(e)}, status=400)
+            return Response({"success": False, "message": str(e), "status": 400}, status=400)
 
     def get(self, request):
         try:
             notes = Notes.objects.filter(isTrash=True)
             serializer = NotesSerializer(notes, many=True)
-            return Response(serializer.data)
+            return Response({"success": True, 'message': 'isTrash retrieve successfully!', 'Data': serializer.data,
+                             'status': 201}, status=201)
         except Exception as e:
             logger.exception(e)
-            return Response({'message': str(e)}, status=400)
+            return Response({"success": False, "message": str(e), "status": 400}, status=400)
--- user_auth/views.py
+++ user_auth/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import login
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from logging_confiq.logger import get_logger
@@ -19,20 +19,21 @@ class CustomUserRegistrationAPIView(APIView):
             serializer = CustomUserRegistrationSerializer(data=request.data)
             serializer.is_valid(raise_exception=True)
             serializer.save()
-            return Response({"message": "Registered Successfully", "data": serializer.data, "status": 201}, status=201)
+            return Response({"success": True, "message": "user Registered Successfully", "data": serializer.data,
+                             "status": 201}, status=201)
         except Exception as e:
             logger.exception(e)
-            return Response({'message': str(e)}, status=400)
-
+            return Response({"success": False, "message": str(e), "status": 400}, status=400)
 
     def get(self, request):
         try:
             user = CustomUser.objects.all()
             serializer = CustomUserRegistrationSerializer(user, many=True)
-            return Response(serializer.data)
+            return Response({"success": True, 'message': 'user Retrieve successfully!', 'Data': serializer.data,
+                             "status": 201}, status=201)
         except Exception as e:
             logger.exception(e)
-            return Response({'message': str(e)}, status=400)
+            return Response({"success": False, "message": str(e), "status": 400}, status=400)
 
     def put(self, request, pk):
         try:
@@ -40,19 +41,20 @@ class CustomUserRegistrationAPIView(APIView):
             serializer = self.serializer_class(user, data=request.data)
             serializer.is_valid()
             serializer.save()
-            return Response({'message': 'user updated successfully!', 'Data': serializer.data})
+            return Response({"success": True, 'message': 'user updated successfully!', 'Data': serializer.data,
+                             "status": 201}, status=201)
         except Exception as e:
             logger.exception(e)
-            return Response({'message': str(e)}, status=400)
+            return Response({"success": False, "message": str(e), "status": 400}, status=400)
 
     def delete(self, request, pk):
         try:
             user = CustomUser.objects.get(id=pk)
             user.delete()
-            return Response({"Message": "User Deleted Successfully"}, status=204)
+            return Response({"success": True, "Message": "User Deleted Successfully"}, status=204)
         except Exception as e:
             logger.exception(e)
-            return Response({'message': str(e)}, status=400)
+            return Response({"success": False, "message": str(e), "status": 400}, status=400)
 
 
 # *********************************user-login Apiview*****************
@@ -67,7 +69,7 @@ class CustomUserLoginAPIView(APIView):
 
             serializer.save()
             login(request, serializer.context.get('user'))
-            return Response({"message": "Login Successfully"}, status=201)
+            return Response({"success": True, "message": "Login Successfully", "status": 201}, status=201)
         except Exception as e:
             logger.exception(e)
-            return Response({'message': str(e)}, status=400)
+            return Response({"success": False, "message": str(e), "status": 400}, status=400)

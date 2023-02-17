8550260a63d627bf85aa2dc96cc8bd034004a272
rajshri-priya <rajshri.priya1708@.gmail.com>
2023-02-17 09:24:43 +0530

    [Rajshri-Priya]refactor  serializer for labels in Notes

--- Notes/serializers.py
+++ Notes/serializers.py
@@ -11,10 +11,34 @@ class LabelsSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Labels
+        fields = ['name', 'user']
+
+
+class DisplayLabelsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Labels
         fields = ['name']
 
 
 class NotesSerializer(serializers.ModelSerializer):
+    label = DisplayLabelsSerializer(many=True, read_only=True)
+
     class Meta:
         model = Notes
-        fields = ['id', 'user', 'title', 'description', 'isArchive', 'isTrash', 'color', 'image']
+        fields = ['id', 'user', 'title', 'description', 'isArchive', 'isTrash', 'color', 'image', 'label',
+                  'collaborator', 'reminder']
+        read_only_fields = ['label','collaborator']
+
+    def create(self, validated_data):
+        print(validated_data)
+        label_name = self.initial_data.get('label')
+        notes = Notes.objects.create(**validated_data)
+        label = Labels.objects.filter(name=label_name)
+        if label.exists():
+            notes.label.add(label.first())
+            return notes
+
+        label = Labels.objects.create(name=label_name, user=validated_data.get("user"))
+        notes.label.add(label)
+        return notes
+

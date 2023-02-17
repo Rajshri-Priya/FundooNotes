a527a0004f2c211ca3f1b3693ae7a316bd81fc5c
rajshri-priya <rajshri.priya1708@.gmail.com>
2023-02-17 09:32:59 +0530

    [Rajshri-Priya] add sendmail() task to send mail for reminder

---
+++ Notes/tasks.py
@@ -0,0 +1,15 @@
+from celery import shared_task
+from django.core.mail import send_mail
+from FundooNotes import settings
+
+
+@shared_task(bind=True)
+def send_mail_func(self, subject, message, recipient_list):
+    send_mail(
+        subject=subject,
+        message=message,
+        from_email=settings.EMAIL_HOST_USER,
+        recipient_list=recipient_list,
+        fail_silently=False,
+    )
+    return "done"

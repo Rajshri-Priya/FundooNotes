4a83fa326f129c825d9ad41e7d56630635e40b69
rajshri-priya <rajshri.priya1708@.gmail.com>
2023-02-20 13:59:53 +0530

    [Rajshri-Priya] refactor create schema_view() for apply swagger

--- FundooNotes/urls.py
+++ FundooNotes/urls.py
@@ -10,16 +10,35 @@ Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.urls import include, path
+    1. Import include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
+# Define the schema view for Swagger
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Fundoo Notes API",
+        default_version='v1',
+        description="Fundoo notes api helps to create, update, delete notes.",
+        # terms_of_service="https://terms/services",
+        contact=openapi.Contact(email="priyagorkha711@gmail.com"),
+        # license=openapi.License(name="License"),
+    ),
+    public=True,
+    permission_classes=([permissions.AllowAny, ]),
+
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('user_auth.urls')),
     path('notes/', include('Notes.urls')),
-
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
+

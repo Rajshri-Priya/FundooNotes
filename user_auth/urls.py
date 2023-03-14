ef39db40a8cfa94abb94c916905ec853c6fcd989
rajshri-priya <rajshri.priya1708@.gmail.com>
2023-03-14 10:42:30 +0530

    [Rajshri-Priya].Added urls for render html pages

--- user_auth/urls.py
+++ user_auth/urls.py
@@ -7,5 +7,10 @@ urlpatterns = [
     path('signup/<int:pk>', views.CustomUserRegistrationAPIView.as_view(), name='signup'),
     path('login/', views.CustomUserLoginAPIView.as_view(), name='login'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
-
+    path('logout_view/', views.Logout.as_view(), name='logout_render'),
+    path('login_view/', views.LoginView.as_view(), name='login_render'),
+    path('register_view/', views.RegistrationView.as_view(), name='register_render'),
+    path('base/', views.base_page, name='base'),
+    path('home/', views.home_page, name='home'),
+    path('profile/', views.LoginView.as_view, name='profile')
 ]

f047306a7e0b9e0c65e417f0b2fb4391a795be9a
rajshri-priya <rajshri.priya1708@.gmail.com>
2023-03-14 10:44:25 +0530

    [Rajshri-Priya].Added views for render html pages

--- user_auth/views.py
+++ user_auth/views.py
@@ -1,6 +1,9 @@
+import requests
 from django.contrib.auth import login, logout
-from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.utils.decorators import method_decorator
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -8,6 +11,7 @@ from rest_framework.views import APIView
 from logging_confiq.logger import get_logger
 from user_auth.models import CustomUser
 from user_auth.serializers import CustomUserRegistrationSerializer, CustomUserLoginSerializer
+from django.views.generic import View
 
 # logging config
 logger = get_logger()
@@ -27,18 +31,21 @@ class CustomUserRegistrationAPIView(APIView):
             serializer = CustomUserRegistrationSerializer(data=request.data)
             serializer.is_valid(raise_exception=True)
             serializer.save()
+            # return render(request, "user_auth/registration_success.html", {"user": serializer.data})
+
             return Response({"success": True, "message": "user Registered Successfully", "data": serializer.data,
                              "status": 201}, status=201)
+
         except Exception as e:
             logger.exception(e)
             return Response({"success": False, "message": str(e), "status": 400}, status=400)
 
     def get(self, request):
         try:
-            user = CustomUser.objects.all()
-            serializer = CustomUserRegistrationSerializer(user, many=True)
+            user = request.user
+            serializer = CustomUserRegistrationSerializer(user)
             return Response({"success": True, 'message': 'user Retrieve successfully!', 'Data': serializer.data,
-                             "status": 201}, status=201)
+                             "status": 200}, status=200)
         except Exception as e:
             logger.exception(e)
             return Response({"success": False, "message": str(e), "status": 400}, status=400)
@@ -83,10 +90,11 @@ class CustomUserLoginAPIView(APIView):
         try:
             serializer = CustomUserLoginSerializer(data=request.data)
             serializer.is_valid(raise_exception=True)
-
             serializer.save()
-            login(request, serializer.context.get('user'))
-            return Response({"success": True, "message": "Login Successfully", "status": 201}, status=201)
+            user = serializer.context.get('user')
+            login(request, user)
+            return Response({"success": True, "message": "Login Successfully", "user": user, "status": 201},
+                            status=201)
         except Exception as e:
             logger.exception(e)
             return Response({"success": False, "message": str(e), "status": 401}, status=401)
@@ -110,3 +118,71 @@ class LogoutView(APIView):
         except Exception as e:
             logger.exception(e)
             return Response({'message': 'An error occurred during logout: {}'.format(str(e))})
+
+
+class LoginView(View):
+    def get(self, request):
+        return render(request, "user_auth/login.html")
+
+    def post(self, request):
+        api_url = 'http://127.0.0.1:8000/user/login/'
+        data = {'username': request.POST['username'], 'password': request.POST['password']}
+        response = requests.post(api_url, data=data)
+        print(dir(response))
+        # Check the response status code and render the appropriate template
+        if response.status_code == 201:
+            response_data = response.json()
+            user = response_data.get('user', '')
+            # context = {'data': user}
+            return render(request, ["user_auth/login_success.html", "user_auth/home.html"], {"user": user})
+            # return redirect('base.html')
+        else:
+            return render(request, "user_auth/login_failure.html")
+
+
+class RegistrationView(View):
+    def get(self, request):
+        return render(request, "user_auth/registeration.html")
+
+    def post(self, request):
+        # Make a POST request to the API endpoint with the form data
+        api_url = 'http://127.0.0.1:8000/user/signup/'
+        data = {'first_name': request.POST['first_name'], 'last_name': request.POST['last_name'],
+                'address': request.POST['address'], 'phone': request.POST['phone'], 'email': request.POST['email'],
+                'username': request.POST['username'], 'password': request.POST.get('password')
+                }
+
+        response = requests.post(api_url, data=data)
+        # Check the response status code and render the appropriate template
+        if response.status_code == 201:
+            return render(request, 'user_auth/register_success.html')
+        else:
+            response_data = response.json()
+            message = response_data.get('message', '')
+            return render(request, 'user_auth/register_failure.html', {'message': message})
+
+
+@method_decorator(login_required, name='dispatch')
+class ProfileView(View):
+    def get(request):
+        # return render(request, "user_auth/profile.html")
+        api_url = 'http://127.0.0.1:8000/user/signup/'
+        response = requests.get(api_url)
+        # Check the response status code and render the appropriate template
+        if response.status_code == 201:
+            response_data = response.json()
+            print(response_data)
+            return render(request, 'user_auth/profile.html')
+
+
+class Logout(View):
+    def get(self, request):
+        return render(request, 'user_auth/logout.html')
+
+
+def base_page(request):
+    return render(request, 'user_auth/base.html')
+
+
+def home_page(request):
+    return render(request, 'user_auth/home.html')
